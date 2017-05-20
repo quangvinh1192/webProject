3193040682c35aae5f7a9245705dfc565ddd285e
Vinh Tran <quangvinh1192@gmail.com>
2017-05-20 14:53:04 -0700

    update some test cases

--- test/unit/testDashboard.js
+++ test/unit/testDashboard.js
@@ -10,3 +10,13 @@ QUnit.test('dateToString Test', function () {
   QUnit.assert.deepEqual(dateToString(date4),"undefined NaN NaN","testing Bad Input 99-99-99999");
 
 });
+
+QUnit.test('checkTime Test', function () {
+   QUnit.assert.equal(checkTime(1),"01","checkTime test: 1");
+   QUnit.assert.equal(checkTime(10),"10","checkTime test: 10");
+   QUnit.assert.equal(checkTime(12),"12","checkTime test: 12");
+   QUnit.assert.equal(getAppDate(date2),"03:59 PM","getAppDate test: 22-11-1131");
+   QUnit.assert.equal(getAppDate(),"aN:aN PM","getAppDate test: no input");
+   QUnit.assert.equal(getAppDate(date1),"aN:aN PM","getAppDate test: invalid input");
+   QUnit.assert.equal(getAppDate(new Date("2015-01-01")),"04:00 PM","getAppDate test: 22-11-1131");
+});
