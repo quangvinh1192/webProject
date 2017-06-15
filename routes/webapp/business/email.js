6ba32d81dc401ed40a759a43c7d6531f9ebe5331
Vinh Tran <quangvinh1192@gmail.com>
2017-06-14 22:04:21 -0700

    try to add mocha

---
+++ routes/webapp/business/email.js
@@ -0,0 +1,31 @@
+'use strict';
+var nodemailer = require('nodemailer');
+
+module.exports.email = function(req,smsBody){
+    var transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true, // use SSL
+        auth: {
+            user:'cse112team8@gmail.com',
+            pass: 'fubarForPeter123'
+        }
+    });
+
+	// setup email data with unicode symbols
+	var mailOptions = {
+		from: 'Enque <cse112team8@gmail>', // sender address
+		to: req.body.inputEmail, // list of receivers
+		subject: 'Appointment Confirmation', // Subject line
+		text: smsBody,
+
+	};
+
+	// send mail with defined transport object
+	transporter.sendMail(mailOptions, (error, info) => {
+		if (error) {
+			console.log(error);
+		}
+	});
+
+}
--- test/test.js
+++ test/test.js
@@ -1,5 +1,146 @@
-describe ("test", function(){
-	it("should work", function () {
-		expect(true).toBe(true)
-	})
-})
+var assert = require('assert');
+describe('First_install', function() {
+  describe('test_assert', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(-1, [1,2,3].indexOf(4));
+    });
+  });
+});
+
+var SMS = require('../routes/webapp/business/sms.js');
+var validation = require('../routes/webapp/business/validation.js');
+var EMAIL = require('../routes/webapp/business/email.js');
+var assert = require('assert');
+var req = {
+    body:{
+    inputName:"test",
+    inputDate:"Monday",
+    inputTime:"20",
+    inputPhone:"+aaa"
+}
+};
+var res = "test";
+// sms(req, res);
+
+var assert = require('assert');
+describe('sms test', function() {
+  describe('failing to send an string aaa', function() {
+    it('wrong number', function() {
+      assert.equal(SMS.sms(req, res), console.log("The 'To' number +aaa is not a valid phone number."));
+    });
+  });
+  describe('failing to send an string aaa', function() {
+    it('invalid number', function() {
+        req.body.inputPhone ="+141547714462"
+        assert.equal(SMS.sms(req, res), console.log("The 'To' number +141547714462 is not a valid phone number."));
+    });
+  });
+
+  describe('failing to send a wrong number', function() {
+      req.body.inputPhone ="123234567"
+      it('send wrong number in to', function() {
+      assert.equal(SMS.sms(req, res), console.log("The 'To' number +123234567 is not a valid phone number."));
+    });
+  });
+  describe('failing to send an empty number', function() {
+    it('wrong number', function() {
+      assert.equal(SMS.sms(req, res), console.log("The 'To' number +1 is not a valid phone number."));
+    });
+  });
+  describe('sending succesfully without body', function() {
+      req.body.inputPhone ="+14154771446";
+      res="";
+    it('right number without message', function() {
+      assert.equal(SMS.sms(req, res), console.log(""));
+    });
+  });
+  describe('sending succesfully without date', function() {
+      req.body.inputPhone ="+14154771446"
+      req.body.inputDate ="";
+    it('sending succesfully without date', function() {
+      assert.equal(SMS.sms(req, res), console.log(""));
+    });
+  });
+  describe('sending succesfully without inputTime', function() {
+      req.body.inputPhone ="+14154771446"
+      req.body.inputTime ="";
+    it('sending succesfully without inputTime', function() {
+      assert.equal(SMS.sms(req, res), console.log(""));
+    });
+  });
+  describe('sending succesfully without inputName', function() {
+      req.body.inputPhone ="+14154771446"
+      req.body.inputName ="";
+    it('sending succesfully without inputName', function() {
+      assert.equal(SMS.sms(req, res), console.log(""));
+    });
+  });
+  describe('sending succesfully', function() {
+      req.body.inputPhone ="+14154771446"
+    it('right number', function() {
+      assert.equal(SMS.sms(req, res), console.log(""));
+    });
+  });
+  describe('sending outside us number', function() {
+      req.body.inputPhone ="+841687854687"
+      it('sending message succesfully using ', function() {
+      assert.equal(SMS.sms(req, res), console.log(""));
+    });
+  });
+  describe('test valid name isValidName: who', function() {
+      it('test valid name isValidName: who', function() {
+      assert.equal(validation.isValidName("who"), true);
+    });
+  });
+  describe('test valid name isValidName: apt with dr', function() {
+      it('test isValidName: apt with dr', function() {
+      assert.equal(validation.isValidName("apt with dr"), true);
+    });
+  });
+  describe('test valid name isValidName: ', function() {
+      it('test isValidName: valid name who', function() {
+      assert.equal(validation.isValidName("apt with dr.j"), false);
+    });
+  });
+  describe('test valid name isValidName: 87*fasfd', function() {
+      it('test isValidName: valid name 87*fasfd', function() {
+      assert.equal(validation.isValidName("87*fasfd"), false);
+    });
+  });
+  describe('test invalid name isValidName: remind time', function() {
+      it('test valid name isValidName: remind time', function() {
+      assert.equal(validation.isValidName("remind time"), true);
+    });
+  });
+  describe('test invalid name isValidName: apt 9', function() {
+      it('test invalid name isValidName: apt 9', function() {
+      assert.equal(validation.isValidName("apt 9"), false);
+    });
+  });
+  describe('test invalid name isValidName: ', function() {
+      it('test invalid name isValidName: ', function() {
+      assert.equal(validation.isValidName(""), false);
+    });
+  });
+  describe('test phone: 4088888888', function() {
+      it('test phone: 4088888888', function() {
+      assert.equal(validation.phone("4088888888"), true);
+    });
+  });
+  describe('test invalid phone: 408888888', function() {
+      it('test invalid phone: 408888888', function() {
+      assert.equal(validation.phone("408888888"), false);
+    });
+  });
+  describe('test invalid phone: 4088a88888', function() {
+      it('test invalid phone: 4088a88888', function() {
+      assert.equal(validation.phone("4088a88888"), false);
+    });
+  });
+  describe('test invalid phone: 4088?88888', function() {
+      it('test invalid phone: 4088?88888', function() {
+      assert.equal(validation.phone("4088?88888"), false);
+    });
+  });
+});
+;
